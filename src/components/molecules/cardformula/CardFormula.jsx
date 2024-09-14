21be8a14a0c45126d25da843b8071ef68feef11f
MiguelGMata <miguelgmata.dev@gmail.com>
2024-09-14 02:19:07 +0200

    creation navbar

--- src/components/molecules/cardformula/CardFormula.jsx
+++ src/components/molecules/cardformula/CardFormula.jsx
@@ -3,15 +3,14 @@ import Images1 from '../../../assets/images/formula1.jpg'
 import Images2 from '../../../assets/images/formula2.jpg'
 import Images3 from '../../../assets/images/formula3.jpg'
 import Images4 from '../../../assets/images/formula4.jpg'
-import './cardFormula.css'; // Importa el archivo de estilos
+import './cardFormula.css'; 
 
 const formulas = [
   { title: "Formule à Volonté", price: "29,90€", imageUrl: Images1 },
   { title: "Formule à Midi", price: "15,90€", imageUrl: Images2},
   { title: "Formule Salée", price: "18,90€", imageUrl: Images3 },
   { title: "Formule P'tit Breton", price: "13,90€", imageUrl: Images4 },
-
-];
+]
 
 const CardFormula = () => {
     return (
